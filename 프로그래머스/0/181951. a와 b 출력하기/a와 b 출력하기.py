bfadcb460dc8d9a0e2f9d1d88c2817043d673cb6
Selena <113491089+songmac@users.noreply.github.com>
2024-05-08 00:58:51 +0900

    [level 0] Title: a와 b 출력하기, Time: 12.54 ms, Memory: 7.57 MB -BaekjoonHub

--- 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.py
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.py
@@ -1,3 +1,9 @@
 a, b = map(int, input().strip().split(' '))
 print("a = %d" % a)
 print("b = %d" % b)
+
+"""
+map(function, iterable)
+: 첫 번째 매개변수로는 함수가 오고,
+두 번째 매개변수로는 반복 가능한 자료형(리스트, 튜플 등)이 옴
+"""
