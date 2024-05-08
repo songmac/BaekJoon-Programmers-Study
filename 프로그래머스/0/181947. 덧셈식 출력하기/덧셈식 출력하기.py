2729dec8b7e76951902821e8143ef30fef8232bb
Selena <113491089+songmac@users.noreply.github.com>
2024-05-09 00:06:45 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 10.69 ms, Memory: 7.54 MB -BaekjoonHub

--- 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.py
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.py
@@ -1,7 +1,11 @@
 a, b = map(int, input().strip().split(' '))
 
-print("{} + {} = {}".format(a, b, a + b))
+
+print(f"{a} + {b} = {a + b}") # f스트링
+
 """
 동일한 표현
+print(a, '+', b, '=', a+b)
 print( "%d + %d = %d" % (a, b, a + b))
+print("{} + {} = {}".format(a, b, a + b))
 """
