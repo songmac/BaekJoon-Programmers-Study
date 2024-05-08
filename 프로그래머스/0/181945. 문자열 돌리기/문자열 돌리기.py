94ba9951a7cac8ecdf7e7af66ec45e2a1013d646
Selena <113491089+songmac@users.noreply.github.com>
2024-05-09 00:22:39 +0900

    [level 0] Title: 문자열 돌리기, Time: 15.78 ms, Memory: 7.36 MB -BaekjoonHub

--- 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.py
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.py
@@ -2,3 +2,10 @@ str = str(input()).strip()
 
 for i in str:
     print(i)
+    
+    
+"""
+다른 풀이
+1.print('\n'.join(input())) : 사용자로부터 입력을 받은 후, 입력받은 문자열의 각 문자 사이에 줄바꿈 문자 (\n)를 삽입하고, 그 결과를 출력
+
+"""
