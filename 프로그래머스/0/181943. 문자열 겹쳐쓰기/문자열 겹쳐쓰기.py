aa472b7675df7abb6eaf0129ca212b7aaaadf0ee
Selena <113491089+songmac@users.noreply.github.com>
2024-05-09 01:19:31 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -2,9 +2,22 @@ def solution(my_string, overwrite_string, s):
     a = len(my_string[:s])
     b = len(overwrite_string)
     answer = my_string[:s] + overwrite_string[:b] + my_string[a+b:]
-    
     return answer
 
+"""
+좀 더 간결한 표현
+answer = my_string[:s] + overwrite_string + my_string[s+len(overwrite_string):]
+"""
 
-            
+
+"""
+다른 풀이
+def solution(my_str, over_str, s):
+    a = my_str
+    b = over_str
+    answer = a[:s] + b[:len(b)] + a[len(a[:s])+len(b):]
+    return answer    
+"""
+
+
             
