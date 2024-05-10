acbea9cbf7ab120edfbfb25fec6320e2f92e1231
Selena <113491089+songmac@users.noreply.github.com>
2024-05-11 00:15:24 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.py
+++ 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.py
@@ -1,5 +1,12 @@
+"""
+기본 풀이
 def solution(arr):
     answer = ''
     for i in arr:
         answer += i
     return answer
+"""
+
+
+def solution(arr):
+    return ''.join(arr) # '구분자'.join(반복가능객체)
