b8f583a0e5d23baa5c1b4c592e6f417fa3ce1b8a
Selena <113491089+songmac@users.noreply.github.com>
2024-05-11 00:28:13 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.py
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.py
@@ -1,6 +1,17 @@
+"""
+기본풀이
 def solution(my_string, k):
     answer = ''
     for i in range(k):
         answer += my_string
     return answer
+"""
 
+def solution(my_string, k):
+    return my_string*k
+
+
+"""
+def solution(my_string, k):
+    return ''.join(my_string)*k
+"""
