cb137ddac51d0001db1baa709c4229ee52080788
Selena <113491089+songmac@users.noreply.github.com>
2025-02-12 00:00:33 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 1.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -1,9 +1,4 @@
 def solution(my_string, queries):
-    
-    str_list = list(my_string)  
-
-    for s, e in queries:
-        str_list[s:e+1] = str_list[s:e+1][::-1]
-    
-    return ''.join(str_list)
-
+    for (s, e) in queries:
+        my_string = my_string[:s] + my_string[s:e+1][::-1] + my_string[e+1:]
+    return my_string
