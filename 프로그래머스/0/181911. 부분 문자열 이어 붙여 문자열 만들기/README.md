d9b3fe9fac33c57d35652244653fb1e228e2d612
Selena <113491089+songmac@users.noreply.github.com>
2025-02-13 23:11:48 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/README.md
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 부분 문자열 이어 붙여 문자열 만들기 - 181911 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181911?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181911) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 1.06 ms
+메모리: 10.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 13일 22:41:14
+2025년 02월 13일 23:11:46
 
 ### 문제 설명
 
--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.py
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.py
@@ -1,11 +1,5 @@
 def solution(my_strings, parts):
-    
-    result=''
-    for i in range(len(my_strings)):
-        s, e = parts[i]  # 리스트 인자 [x, y]를 각각 s, e에 할당연산자 통해 할당함
-        # print(s, e)  
-        # print(type(s), type(e)) # (int, int)
-        for j in range(s, e+1, 1):
-            result += my_strings[i][j]
-            
-    return result
+    answer = ''
+    for my_str, (s, e) in zip(my_strings, parts):
+        answer += my_str[s: e+1]
+    return answer
