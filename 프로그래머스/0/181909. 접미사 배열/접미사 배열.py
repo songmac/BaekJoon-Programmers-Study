b1c53a3be7ca0e83cefe30b07a5443d821d9af9e
Selena <113491089+songmac@users.noreply.github.com>
2025-02-16 01:00:27 +0900

    [level 0] Title: 접미사 배열, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
@@ -1,3 +1,2 @@
 def solution(my_string):
-    result = [my_string[len(my_string)-i:] for i in range(1, len(my_string)+1)]
-    return sorted(result)
+    return sorted([my_string[len(my_string)-i:] for i in range(1, len(my_string)+1)])
