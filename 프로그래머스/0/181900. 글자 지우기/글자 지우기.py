03da495f60007ba1c23de6d9f5950f7128c9ad7e
Selena <113491089+songmac@users.noreply.github.com>
2025-02-27 22:10:27 +0900

    [level 0] Title: 글자 지우기, Time: 0.13 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
@@ -1,5 +1,6 @@
 def solution(my_string, indices):
-    indices = sorted(indices)
-    result = ''.join([char for i, char in enumerate(my_string) if i not in indices])
-    
+    result = ''
+    for i, char in enumerate(my_string):
+        if i not in indices:
+            result += char
     return result
