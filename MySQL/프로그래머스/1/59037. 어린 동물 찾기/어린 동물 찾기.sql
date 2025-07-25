8be2441cc473326feaae70891eb7b93b64f4fdbf
Selena <113491089+songmac@users.noreply.github.com>
2025-07-25 19:08:58 +0900

    [level 1] Title: 어린 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE NOT INTAKE_CONDITION = 'Aged'
+ # AND (NAME = 'Diablo' OR NAME = 'Miller' OR NAME = 'Cherokee') 
+ORDER BY ANIMAL_ID ASC
+;
