f4a9c9484ee92d5b159a2c696c408aaa0b9f9f5e
Selena <113491089+songmac@users.noreply.github.com>
2025-08-02 02:23:14 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.15 ms, Memory: 9.3 MB -BaekjoonHub

--- Python3/프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
+++ Python3/프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
@@ -1,8 +1,2 @@
-def solution(myString, pat):
-    if len(myString) < len(pat):
-        return 0
-    else :
-        if pat.lower() in myString.lower():  
-            return 1
-        else : 
-            return 0
+def solution(myString, pat): 
+    return int(pat.lower() in myString.lower())
