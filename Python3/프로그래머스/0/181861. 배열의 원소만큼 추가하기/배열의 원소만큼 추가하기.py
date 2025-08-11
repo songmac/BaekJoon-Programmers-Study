ab3c3e32783b70574bf8118e2a45479b46a18546
songmac <ssy4784@naver.com>
2025-08-11 20:01:46 +0900

    edit : add resolution using list comprehension

--- Python3/프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.py
+++ Python3/프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.py
@@ -1,6 +1,10 @@
+# def solution(arr):
+#     X = []
+#     for a in arr:
+#         for i in range(a):
+#             X.append(a)
+#     return X
+
+
 def solution(arr):
-    X = []
-    for a in arr:
-        for i in range(a):
-            X.append(a)
-    return X
+    return [a for a in arr for i in range(a)]
