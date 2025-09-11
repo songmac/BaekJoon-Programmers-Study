83d36f576cb136b54cbe3c1df2fbabcf9e400ddf
songmac <ssy4784@naver.com>
2025-09-11 23:49:26 +0900

    fix : turn into a comment in line 3

--- Python3/프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.py
+++ Python3/프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.py
@@ -1,6 +1,6 @@
 def solution(arr):
     n_max = len(arr) if len(arr) > len(arr[0]) else (len(arr[0]))
-    n_max = max(len(arr), len(arr[0]))
+    # n_max = max(len(arr), len(arr[0]))
     n_arr = [[0 for _ in range(n_max)] for _ in range(n_max)]
     
     for i in range(len(arr)):
