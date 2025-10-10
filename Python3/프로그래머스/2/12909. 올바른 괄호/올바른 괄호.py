a17d6488c0d7ce9b4865a6f8ad48be78a1e18b1c
Selena <113491089+songmac@users.noreply.github.com>
2025-10-10 18:31:37 +0900

    [level 2] Title: 올바른 괄호, Time: 15.98 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ Python3/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,3 +1,4 @@
+"""
 def solution(s):
     balance = 0  # 균형을 확인하는 변수
     for char in s:  # 문자열 s의 각 문자를 순회
@@ -8,3 +9,27 @@ def solution(s):
             if balance < 0:  # balance가 음수이면 닫는 괄호가 더 많음
                 return False  # 짝이 맞지 않으므로 False 반환
     return balance == 0  # balance가 0이면 짝이 맞으므로 True, 아니면 False
+"""
+
+def solution(s):
+    """주어진 문자열의 괄호 짝이 맞는지 확인합니다."""
+    stack = []
+    num_stack = []
+    
+    for char in s:
+        if char == "(":
+            stack.append(char)
+            num_stack.append(1)
+            
+        elif char == ")":
+            if len(stack) == 0:
+                num_stack.append(-1)
+                continue
+            else:
+                stack.pop()
+                num_stack.append(-1)
+    
+    if (len(stack) == 0) and (sum(num_stack) == 0) and (num_stack[0] != -1):
+        return True
+    else:
+        return False
